8e52c80814ef0f3878a5ac01f57c394ba1ea4391
Gaicugi <95116161+Gaicugi@users.noreply.github.com>
2026-03-06 03:30:35 +0300

    Create imports.py

---
+++ imports.py
@@ -0,0 +1,8 @@
+import imaplib
+import email
+import pandas as pd
+import re
+from email.header import decode_header
+from PyPDF2 import PdfFileReader
+from datetime import datetime, timedelta
+from io import BytesIO
